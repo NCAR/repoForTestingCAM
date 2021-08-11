33c0d6d224463d07baab21b07e0ad17be3426cc1
Jesse Nusbaumer <nusbaume@ucar.edu>
2021-08-11 09:39:45 -0600

    Fix egregious doctest error.

--- cime_config/cam_config.py
+++ cime_config/cam_config.py
@@ -793,7 +793,7 @@ class ConfigCAM:
         Traceback (most recent call last):
         SystemExit: 2
         >>> ConfigCAM.parse_config_opts("--physics-suites kessler")
-        What is going on here????!?!?!??!?!
+        Namespace(dyn='', physics_suites='kessler')
         >>> ConfigCAM.parse_config_opts("--physics-suites kessler --dyn se")
         Namespace(dyn='se', physics_suites='kessler')
         >>> ConfigCAM.parse_config_opts("--physics-suites kessler --dyn se")
